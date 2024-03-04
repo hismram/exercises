7ab2f993e073275660207bb2da2424dd12214040
rr.hismatullin <hismram@gmail.com>
2024-03-04 23:51:47 +0500

    Решение "в лоб"

---
+++ leetcode/1512. Number of Good Pairs/Java/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int pairs = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 1 < nums.length) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] == nums[j]) {
+                        pairs++;
+                    }
+                }
+            }
+        }
+
+        return pairs;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().numIdenticalPairs(new int[]{1,2,3}));
+    }
+}
